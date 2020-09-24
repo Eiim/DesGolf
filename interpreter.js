87f3a899ee11983a3f96710a204163486b4adbb5
Eiim <ethan@we3chapmans.com>
2020-09-24 14:48:24 -0400

    Refactor function handling for legibility and efficiency

--- interpreter.js
+++ interpreter.js
@@ -102,39 +102,47 @@ function parseTree(tree) {
 	// Functions will be checked here
 	for(var i = 0; i < tree.length; ++i) {
 		var op = tree[i];
-		if(typeof(op) == "string" && op == "q") {
-			if(i+1<tree.length && tree[i+1] instanceof Decimal) {
-				tree.splice(i, 2, tree[i+1].sqrt());
-			}
-		} else if(typeof(op) == "string" && op == "s") {
-			if(i+1<tree.length && tree[i+1] instanceof Decimal) {
-				tree.splice(i, 2, tree[i+1].sin());
-			}
-		} else if(typeof(op) == "string" && op == "c") {
-			if(i+1<tree.length && tree[i+1] instanceof Decimal) {
-				tree.splice(i, 2, tree[i+1].cos());
-			}
-		} else if(typeof(op) == "string" && op == "t") {
-			if(i+1<tree.length && tree[i+1] instanceof Decimal) {
-				tree.splice(i, 2, tree[i+1].tan());
-			}
-		} else if(typeof(op) == "string" && op == "!") {
-			if(i+1<tree.length && tree[i+1] instanceof Decimal) {
-				var n = tree[i+1];
-				if(n.isInt()) {
-					var o = 1;
-					for(var it = 2; it <= n.valueOf(); ++it) {
-						o *= it;
+		if(typeof(op) == "string") {
+			switch (op) {
+				case "q":
+					if(i+1<tree.length && tree[i+1] instanceof Decimal) {
+						tree.splice(i, 2, tree[i+1].sqrt());
 					}
-					tree.splice(i, 2, new Decimal(o));
-				} else {
-					//TODO: Implement
-					var o = 1;
-					for(var it = 2; it < n.valueOf(); ++it) {
-						o *= it;
+					break;
+				case "s":
+					if(i+1<tree.length && tree[i+1] instanceof Decimal) {
+						tree.splice(i, 2, tree[i+1].sin());
 					}
-					tree.splice(i, 2, new Decimal(o));
-				}
+					break;
+				case "c":
+					if(i+1<tree.length && tree[i+1] instanceof Decimal) {
+						tree.splice(i, 2, tree[i+1].cos());
+					}
+					break;
+				case "t":
+					if(i+1<tree.length && tree[i+1] instanceof Decimal) {
+						tree.splice(i, 2, tree[i+1].tan());
+					}
+					break;
+				case "!":
+					if(i+1<tree.length && tree[i+1] instanceof Decimal) {
+						var n = tree[i+1];
+						if(n.isInt()) {
+							var o = 1;
+							for(var it = 2; it <= n.valueOf(); ++it) {
+								o *= it;
+							}
+							tree.splice(i, 2, new Decimal(o));
+						} else {
+							//TODO: Implement
+							var o = 1;
+							for(var it = 2; it < n.valueOf(); ++it) {
+								o *= it;
+							}
+							tree.splice(i, 2, new Decimal(o));
+						}
+					}
+				break;
 			}
 		}
 	}
